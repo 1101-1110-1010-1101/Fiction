795c166343c5e56559c22f1a7eaf0a93ea789686
Ivan Sarzhevskiy <giomail.iv@gmail.com>
2018-03-23 02:15:28 +0300

    Some new features

--- Fiction.py
+++ Fiction.py
@@ -10,7 +10,9 @@ vk = vk_api.VkApi(token=api_token)
 print('Connecting...')
 vk._auth_token()
 print('Connected successfully!\nFiction is running')
-
+date_n = str(datetime.datetime.today().date())
+log_file = open('C:\\Users\\User\\Desktop\\Проекты\\Fiction\\logs\\' + str(date_n) + ".log", 'a')
+log_file.write('Initialized at ' + str(datetime.datetime.now().time()) + '\n')
 
 values = {'out':0, 'count':100, 'time_offset':10}
 response = vk.method('messages.get', values)
@@ -18,14 +20,31 @@ answers = ['Да', 'Нет', 'Тебе бы это понравилось, ', ''
            'Не расстраивайся по этому поводу - лучше напиши фанфик',
            'Следуй за ветром', 'Возможно, в следующей жизни', 'Отнюдь!']
 id_name = {}
+stats = {}
 
 
 def fetch_user(user_id):
     name = id_name.get(user_id)
     if name is None:
         id_name[user_id] = vk.method('users.get', {'user_id': user_id})[0]['first_name']
-        print(id_name[user_id])
     return id_name[user_id]
+
+
+def count_msgs(user_id):
+    id = stats.get(user_id)
+    if id is None:
+        stats[user_id] = 1
+    else:
+        stats[user_id] += 1
+
+
+def show_stats():
+    result = ''
+    for i in stats.keys():
+        result += vk.method('users.get', {'user_id': i})[0]['first_name'] + ' ' +\
+                  vk.method('users.get', {'user_id': i})[0]['last_name'] + ': ' +\
+                  str(stats.get(i) - 1) + '\n'
+    return result
 
 
 def write_msg(user_id, s):
@@ -33,7 +52,7 @@ def write_msg(user_id, s):
 
 
 def get_markov_chain():
-    with open('C:\\Users\\User\\Desktop\\ti_uzhe_vozbudilsja__potter.txt', encoding='utf8') as f:
+    with open('C:\\Users\\User\\Desktop\\ITMO\\source.txt') as f:
         text = f.read()
     return markovify.Text(text)
 
@@ -45,7 +64,10 @@ while True:
     if response['items']:
         values['last_message_id'] = response['items'][0]['id']
     for item in response['items']:
+        fetch_user(item['user_id'])
         print('Got message from ' + str(vk.method('users.get', {'user_id': item['user_id']})) + str(datetime.datetime.now()))
+        count_msgs(item['user_id'])
+        log_file.write(('Got message from ' + str(vk.method('users.get', {'user_id': item['user_id']})) + str(datetime.datetime.now()) + '\n'))
         if response['items'][0]['body'] == 'Кто ты?':
             write_msg(item['user_id'], 'Я Фикшн, приятно познакомиться, ' + fetch_user(item['user_id']))
         elif response['items'][0]['body'] == 'О, всевышний!':
@@ -65,9 +87,29 @@ while True:
                     else:
                         write_msg(item['user_id'], answers[r])
                         break
-        elif response['items'][0]['body'] == 'Дядя Федя!':
-            crime_and_punishment = str(text.make_sentence()) + " " + str(text.make_sentence())
+        elif response['items'][0]['body'] == 'Расскажи историю':
+            crime_and_punishment = ''
+            for i in range(5):
+                a = 'None '
+                while a == 'None ':
+                    a = str(text.make_sentence()) + ' '
+                crime_and_punishment += a
             write_msg(item['user_id'], crime_and_punishment)
+        elif response['items'][0]['body'] == '\\fic.save_log':
+            if item['user_id'] == 155703829:
+                log_file.close()
+                log_file = open('C:\\Users\\User\\Desktop\\Проекты\\Fiction\\logs\\' + str(date_n) + ".log", 'a')
+            else:
+                write_msg(item['user_id'], 'Извините, не хватает прав для выполнения команды')
+        elif response['items'][0]['body'] == '\\fic.exit':
+            if item['user_id'] == 155703829:
+                write_msg(item['user_id'], 'До встречи, ' + fetch_user(item['user_id']))
+                log_file.close()
+                exit(0)
+            else:
+                write_msg(item['user_id'], 'Извините, не хватает прав для выполнения команды')
+        elif response['items'][0]['body'] == '\\fic.stats':
+            write_msg(item['user_id'], show_stats())
         else:
             write_msg(item['user_id'], 'Я не понимаю тебя')
 
